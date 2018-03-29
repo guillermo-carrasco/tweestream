d21783623d9f6b878a16017c6292319c0de4c702
Guillermo Carrasco <guillermo.hernandez@izettle.com>
2018-03-29 16:03:19 +0200

    Added schedule subcommand

--- twistream/cli/cli.py
+++ twistream/cli/cli.py
@@ -1,19 +1,39 @@
 import click
 import os
 
+from dateutil.parser import parse
+
 from twistream.log import logging
 
 LOG = logging.get_logger()
 
 
-@click.command()
-@click.option('--log-level',
-              type=click.Choice(['INFO', 'DEBUG', 'ERROR']),
+def validate_date(ctx, args, date):
+    try:
+        return parse(date)
+    except ValueError:
+        raise click.BadParameter('Dates should be in format YY-MM-DDTHH:MM:SS')
+
+
+@click.group(invoke_without_command=True)
+@click.option('--log-level', type=click.Choice(['INFO', 'DEBUG', 'ERROR']),
               default='INFO')
-@click.option('--hashtags',
-              type=click.STRING,
+@click.option('--hashtags', type=click.STRING,
               help='Comma separated list of hashtags to follow (exclude #)')
-def main(log_level, hashtags):
+@click.pass_context
+def main(ctx, log_level, hashtags):
     LOG.setLevel(log_level)
+    ctx.obj = {
+        'log_level': log_level,
+        'hashtags':hashtags
+    }
     hashtags = [h for h in hashtags.split(',')] if hashtags is not None else []
     LOG.debug(f"Using hashtags: {', '.join(hashtags)}")
+
+
+@main.command(help='Schedule when to read from Twitter Stream API')
+@click.argument('from-date', callback=validate_date,)
+@click.argument('to-date', callback=validate_date)
+@click.pass_context
+def schedule(ctx, from_date, to_date):
+    print(ctx.obj)
