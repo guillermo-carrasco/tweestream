1e40e8f6f1e1a6063fb8cb7a80be8bece940a7e3
guillermo-carrasco <guillermo.carrasco@izettle.com>
2020-10-02 20:41:03 +0200

    Add simple script to get an API object

---
+++ utils/get_api.py
@@ -0,0 +1,21 @@
+"""
+Simple method to quickly get an API object, already authenticated, to work with interactively. It expects a config.yaml
+file in the same directory where it is ran. This yaml file should contain twitter application credentials in the same
+format expected by twistream. In fact, you can very well use the same configuration file.
+"""
+import yaml
+
+from tweepy import API
+from twistream.twitter import client
+
+
+def get_api() -> API:
+    with open('config.yaml', 'r') as f:
+        config = yaml.load(f, Loader=yaml.FlowMappingEndToken)
+
+    return client.get_api(
+        config.get("twitter").get("consumer_key"),
+        config.get("twitter").get("consumer_secret"),
+        config.get("twitter").get("access_token"),
+        config.get("twitter").get("access_token_secret"),
+    )
